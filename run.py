22b6691a60dba50faabd048e407091cc166203a7
Neil Fernandes <neil.nitk@gmail.com>
2020-05-05 17:18:38 +0000

    ADD: better time reporting

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 from timeit import default_timer as timer
 import xgboost as xgb
 
-NUM_LOOPS = 10
+NUM_LOOPS = 1000
+
+def calculate_stats(time_list):
+    """Calculate mean and standard deviation of a list"""
+    mean = sum(time_list) / len(time_list) 
+    variance = sum([((x - mean) ** 2) for x in time_list]) / len(time_list) 
+    std_dev = variance ** 0.5
+    return (mean, std_dev)
 
 data = xgb.DMatrix('data/data.xgb')
 num_rows = data.num_row()
@@ -18,8 +25,14 @@ for i in range(NUM_LOOPS):
     end_time = timer()
 
     total_time = end_time - start_time
-    run_times.append(total_time)
-    inference_time = total_time*1000/num_rows
+    run_times.append(total_time*10e3)
+    inference_time = total_time*(10e6)/num_rows
     inference_times.append(inference_time)
 
     print(f'Loop #{i}', f'{total_time} s', f'\t{inference_time} ms')
+
+mean, std_dev = calculate_stats(run_times)
+print(f"Loops: {len(run_times)} | Time per loop {mean:.2f}ms | Standard Deviation {std_dev:.2f}ms")
+
+mean, std_dev = calculate_stats(inference_times)
+print(f"Time per inference {mean:.2f}\u03bcs | Standard Deviation {std_dev:.2f}\u03bcs")
